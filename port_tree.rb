dde9140ed729735488736791253dfc8c2f5870dd
Tony Doan <tdoan@tdoan.com>
2009-03-13 10:16:32 -0700

    Another rework of this program. It was reopening what is now a class in the Ports module. This has been removed. Previous the Ports::Port class had been added and broke using alternate receipt_paths. This is been fixed.

--- port_tree.rb
+++ port_tree.rb
@@ -19,31 +19,54 @@ require 'erb'
 require File.dirname(__FILE__) + "/lib/port_upgrade.rb"
 require 'sqlite3'
 
-class PortTree
-  
-  def initialize(nodes,deps)
-    throw "Input data must be an Array of tuples" unless deps.is_a? Array
-    @nodes = nodes
-    @deps = deps
+class NodeOptions
+  def initialize(port,args={})
+    case port
+    when Ports::Port
+      name = port.name
+      $stderr.puts "Got a Ports::Port"
+    else
+      port = Ports::Port.new(port)
+      name = port.name
+    end
+    @options = {}
+    @options['label'] = "\"#{name}\""
+    @options['color'] = 'red'
+    @options = @options.merge(args)
+    if port.versions.size > 1
+      @options['shape'] = 'record'
+      @options['color'] = 'purple'
+      @options['label'] = "\"#{name}|{#{port.versions.join("|")}}\""
+    end
   end
   
-  def to_dot
-    template = ERB.new(File.read("port_tree.erb"))
-    tree_data = @deps.collect{|o| [o[0].dot_clean,o[1].dot_clean]}.collect{|p| p[1]==""? p[0]:p.join("->")}.sort{|x,y| x <=> y}.join("\n")
-    ports = @ports
-    deps = @deps.collect{|o| o[1]}.uniq
-    leaves = (@nodes - deps).sort
-    onodes = (@nodes - leaves).sort
-    tree_data += "\n"
-    leaves.each{|p| tree_data += "#{p.dot_clean}[label=\"#{p}\" color=green]\n"}
-    onodes.each{|p| tree_data += "#{p.dot_clean}[label=\"#{p}\" color=red]\n"}
-    template.result(binding)
-   end
+  def []=(key,value)
+    @options[key] = value
+  end
+  
+  def to_s
+    @options.keys.collect do |key|
+      "#{key}=#{@options[key]}"
+    end.join(" ")
+  end
+end
+
+def to_dot(ports,deps)
+  template = ERB.new(File.read("port_tree.erb"))
+  tree_data = deps.collect{|o| [o[0].dot_clean,o[1].dot_clean]}.collect{|p| p[1]==""? p[0]:p.join("->")}.sort{|x,y| x <=> y}.join("\n")
+  #ports = @ports
+  deps = deps.collect{|o| o[1]}.uniq
+  leaves = (ports - deps).sort
+  onodes = (ports - leaves).sort
+  tree_data += "\n"
+  leaves.each{|p| tree_data += "#{p.dot_clean}[#{NodeOptions.new(@pdb[p],"color"=>"green").to_s}]\n" }# "#{p.dot_clean}[label=\"#{p}\" color=green]\n"}
+  onodes.each{|p| tree_data += "#{p.dot_clean}[#{NodeOptions.new(@pdb[p]).to_s}]\n" }
+  template.result(binding)
 end
 
 if __FILE__ == $PROGRAM_NAME
-  pdb = Ports::PortsDB.new(ARGV[0])
-  db = pdb.db
+  @pdb = Ports::PortsDB.new(ARGV[0])
+  db = @pdb.db
   deps = nil
   db.query("select port,dep from deps") do |r|
     deps = r.to_a
@@ -53,6 +76,5 @@ if __FILE__ == $PROGRAM_NAME
     ports = r.to_a.flatten
   end
   db.close
-  pt = PortTree.new(ports,deps)
-  puts pt.to_dot
+  puts to_dot(ports,deps)
 end
