672a20fdd809b38698dadb8bbaee27032189bf29
Tony Doan <tdoan@tdoan.com>
2009-03-12 17:26:02 -0700

    Final commit for 0.1.0

--- lib/port_upgrade.rb
+++ lib/port_upgrade.rb
@@ -29,7 +29,7 @@ class String
 end
 
 module Ports
-  VERSION = '0.0.5'
+  VERSION = '0.1.0'
   RECEIPT_PATH = '/opt/local/var/macports/receipts'
   MACPORTS_DB='/opt/local/var/macports/sources/rsync.macports.org/release/ports'
   CONFIG_FILE = 'port_upgrade.conf'
